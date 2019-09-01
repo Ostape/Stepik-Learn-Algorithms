ea58e5fedaf4234249ff590a0adfa71a29ddabfe
Orest Shemelyuk <orestshemelyuk@gmail.com>
2019-09-01 23:32:12 +0300

    created gcd function with Evklid algorithm

--- main/java/com/robosh/Main.java
+++ main/java/com/robosh/Main.java
@@ -1,7 +1,12 @@
 package com.robosh;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        long a = scanner.nextLong();
+        long b = scanner.nextLong();
+        System.out.println(TaskGCD.gcd(18 ,35));
     }
 }
